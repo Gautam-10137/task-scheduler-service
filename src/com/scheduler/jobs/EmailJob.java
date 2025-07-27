03c3f67b52db0cf725f748308ed79ae98b5592a8
Gautam-10137 <pahwagautam786@gmail.com>
2025-07-27 14:41:13 +0530

    different jobs added

---
+++ src/com/scheduler/jobs/EmailJob.java
@@ -0,0 +1,31 @@
+package com.scheduler.jobs;
+
+import java.util.concurrent.CountDownLatch;
+
+import com.scheduler.core.AbstractJob;
+import com.scheduler.core.JobStatus;
+
+public class EmailJob extends AbstractJob{
+	
+	private final String recipient;
+	
+	public EmailJob(String recipient,int priority, int maxRetryCount,CountDownLatch latch) {
+		super(priority, maxRetryCount,latch);
+		this.recipient=recipient;
+	}
+
+	@Override
+	public void run() {
+		
+		this.setStatus(com.scheduler.core.JobStatus.RUNNING);
+        System.out.println("[EmailJob] Sending email to: " + recipient);
+        if (Math.random() < 0.4) {
+            throw new RuntimeException("Simulated email failure");
+        }
+        System.out.println("[EmailJob] Email sent successfully to: " + recipient);
+        this.setStatus(com.scheduler.core.JobStatus.SUCCESS);
+		
+	}
+
+	
+}
---
+++ src/com/scheduler/jobs/ReportGenerationJob.java
@@ -0,0 +1,35 @@
+package com.scheduler.jobs;
+
+import java.util.concurrent.CountDownLatch;
+import com.scheduler.core.AbstractJob;
+import com.scheduler.core.JobStatus;
+
+public class ReportGenerationJob extends AbstractJob {
+
+    private final String reportName;
+
+    public ReportGenerationJob(String reportName, int priority, int maxRetries, CountDownLatch latch) {
+        super(priority, maxRetries, latch);
+        this.reportName = reportName;
+    }
+
+    @Override
+    public void run() {
+        this.setStatus(JobStatus.RUNNING);
+        System.out.println("[ReportGenerationJob] Generating report: " + reportName);
+
+        // Simulate long-running report creation
+        try {
+            Thread.sleep(1000 + (int)(Math.random() * 1000));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        if (Math.random() < 0.3) {
+            throw new RuntimeException("Report generation failed");
+        }
+
+        System.out.println("[ReportGenerationJob] Report generated: " + reportName);
+        this.setStatus(JobStatus.SUCCESS);
+    }
+}
